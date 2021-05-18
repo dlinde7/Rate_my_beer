6a2e2eb47eb2872c51c13b38c828bc16f96eef84
Waldo Kilian <waldokilian2@gmail.com>
2021-05-18 19:24:19 +0200

    Added Js and made some modifications

---
+++ public/js/product_page.js
@@ -0,0 +1,40 @@
+function onClick() {
+  const products = [
+    {
+      name: "Beer Name",
+      description:
+        "It’s about making placeholder text great again. That’s what people want, they want placeholder text to be great again. If Trump Ipsum weren’t my own words, perhaps I’d be dating it. Lorem Ipsum better hope that there are no 'tapes' of our conversations before he starts leaking to the press!",
+    },
+    {
+      name: "Beer Name",
+      description: "My text is long and beautiful, as, it has been well documented, are various other parts of my website. I think the only difference between me and the other placeholder text is that I’m more honest and my words are more beautiful. An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud.",
+    },
+    {
+      name: "Beer Name",
+      description: "Look at that text! Would anyone use that? Can you imagine that, the text of your next webpage?! An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud. An 'extremely credible source' has called my office and told me that Lorem Ipsum's birth certificate is a fraud.",
+    },
+    {
+      name: "Beer Name",
+      description: "When other websites give you text, they’re not sending the best. They’re not sending you, they’re sending words that have lots of problems and they’re bringing those problems with us. They’re bringing mistakes. They’re bringing misspellings. They’re typists… And some, I assume, are good words.",
+    },
+  ];
+  createElementsWithInnerHTML(products, "Search results");
+}
+
+function createElementsWithInnerHTML(products, message) {
+  const productPlaceholder = document.getElementById("pageContent");
+  const header = document.getElementById("headerText");
+
+  console.log(products);
+  const rows = products.map((product) => {
+    return `<section class="card slit-in-vertical">
+                <section class="content">
+                    <section class="name">${product.name}</section>
+                    <section class="description">${product.description}</section>
+                </section>
+           </section>`;
+  });
+  const html = `${rows.join(" ")}`;
+  productPlaceholder.innerHTML = html;
+  header.innerText = message;
+}
