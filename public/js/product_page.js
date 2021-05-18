2d5fdc099a0f7c159aa81322e7c7eabe2d2699e7
Waldo Kilian <waldokilian2@gmail.com>
2021-05-18 20:35:34 +0200

    Minor bug fixes

--- public/js/product_page.js
+++ public/js/product_page.js
@@ -21,6 +21,70 @@ function onClick() {
       rating: 2,
       description: "When other websites give you text, they’re not sending the best. They’re not sending you, they’re sending words that have lots of problems and they’re bringing those problems with us. They’re bringing mistakes. They’re bringing misspellings. They’re typists… And some, I assume, are good words.",
     },
+    ,
+    {
+      name: "Beer Name",
+      rating: 5,
+      description: "My text is long and beautiful, as, it has been well documented, are various other parts of my website. I think the only difference between me and the other placeholder text is that I’m more honest and my words are more beautiful. An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud.",
+    },
+    {
+      name: "Beer Name",
+      rating: 3,
+      description: "Look at that text! Would anyone use that? Can you imagine that, the text of your next webpage?! An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud. An 'extremely credible source' has called my office and told me that Lorem Ipsum's birth certificate is a fraud.",
+    },
+    {
+      name: "Beer Name",
+      rating: 2,
+      description: "When other websites give you text, they’re not sending the best. They’re not sending you, they’re sending words that have lots of problems and they’re bringing those problems with us. They’re bringing mistakes. They’re bringing misspellings. They’re typists… And some, I assume, are good words.",
+    },
+    ,
+    {
+      name: "Beer Name",
+      rating: 5,
+      description: "My text is long and beautiful, as, it has been well documented, are various other parts of my website. I think the only difference between me and the other placeholder text is that I’m more honest and my words are more beautiful. An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud.",
+    },
+    {
+      name: "Beer Name",
+      rating: 3,
+      description: "Look at that text! Would anyone use that? Can you imagine that, the text of your next webpage?! An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud. An 'extremely credible source' has called my office and told me that Lorem Ipsum's birth certificate is a fraud.",
+    },
+    {
+      name: "Beer Name",
+      rating: 2,
+      description: "When other websites give you text, they’re not sending the best. They’re not sending you, they’re sending words that have lots of problems and they’re bringing those problems with us. They’re bringing mistakes. They’re bringing misspellings. They’re typists… And some, I assume, are good words.",
+    },
+    ,
+    {
+      name: "Beer Name",
+      rating: 5,
+      description: "My text is long and beautiful, as, it has been well documented, are various other parts of my website. I think the only difference between me and the other placeholder text is that I’m more honest and my words are more beautiful. An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud.",
+    },
+    {
+      name: "Beer Name",
+      rating: 3,
+      description: "Look at that text! Would anyone use that? Can you imagine that, the text of your next webpage?! An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud. An 'extremely credible source' has called my office and told me that Lorem Ipsum's birth certificate is a fraud.",
+    },
+    {
+      name: "Beer Name",
+      rating: 2,
+      description: "When other websites give you text, they’re not sending the best. They’re not sending you, they’re sending words that have lots of problems and they’re bringing those problems with us. They’re bringing mistakes. They’re bringing misspellings. They’re typists… And some, I assume, are good words.",
+    },
+    ,
+    {
+      name: "Beer Name",
+      rating: 5,
+      description: "My text is long and beautiful, as, it has been well documented, are various other parts of my website. I think the only difference between me and the other placeholder text is that I’m more honest and my words are more beautiful. An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud.",
+    },
+    {
+      name: "Beer Name",
+      rating: 3,
+      description: "Look at that text! Would anyone use that? Can you imagine that, the text of your next webpage?! An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud. An 'extremely credible source' has called my office and told me that Lorem Ipsum's birth certificate is a fraud.",
+    },
+    {
+      name: "Beer Name",
+      rating: 2,
+      description: "When other websites give you text, they’re not sending the best. They’re not sending you, they’re sending words that have lots of problems and they’re bringing those problems with us. They’re bringing mistakes. They’re bringing misspellings. They’re typists… And some, I assume, are good words.",
+    },
   ];
   createElementsWithInnerHTML(products, "Search results");
 }
