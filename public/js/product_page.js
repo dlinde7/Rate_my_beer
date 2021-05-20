1c9591731233a66d168da0142957ffdaf8ba0d6b
Waldo Kilian <waldokilian2@gmail.com>
2021-05-20 19:20:15 +0200

    updated

--- public/js/product_page.js
+++ public/js/product_page.js
@@ -1,24 +1,31 @@
 function loadBeers() {
-  fetch('/getreviews').then(response => response.json()).then(data => createElementsWithInnerHTML(data,"Featured Beer"));
+  fetch("/getreviews")
+    .then((response) => response.json())
+    .then((data) => createElementsWithInnerHTML(data, "Featured Beer"));
 }
 
 function createElementsWithInnerHTML(products, message) {
   const productPlaceholder = document.getElementById("pageContent");
   const header = document.getElementById("headerText");
-
+  console.log(products);
   const rows = products.map((product) => {
-    return `<section class="card slit-in-vertical">
-                <section class="content">
-                    <section class="name">${product.name}</section>
-                    <section class="rating"> Rating: ${product.rating}</section
-                    <section class="description">${product.description}</section>
-                </section>
-           </section>`;
+    return `<section data-id="${product.idBeer}" onclick="onCardClicked(this)" class="card slit-in-vertical">
+              <section class="content">
+                <section class="name">${product.Name}</section>
+                <section class="rating"> Rating: ${product.Rating}</section>
+                <section class="description">${product.Decription}</section>
+              </section>
+            </section>`;
   });
   const html = `${rows.join(" ")}`;
   productPlaceholder.innerHTML = html;
   header.innerText = message;
 }
-loadBeers()
+loadBeers();
+
+function onCardClicked(card) {
+  var id = card.getAttribute("data-id");
+  console.log(id);
+}
 //SELECT * FROM ratemybeer.Beer where idCompany = {id.idCompany};
 //SELECT * FROM ratemybeer.Beer where Rating => {value};
