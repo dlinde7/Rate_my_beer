68451e0a30b3a51bec80df9135d5085954340a1f
Danie Linde <danielin@bbd.co.za>
2021-05-20 12:37:04 +0200

    app.js update

--- app.js
+++ app.js
@@ -8,13 +8,21 @@ app.use('/public', express.static(__dirname + '/public'));
 app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req, res) {
-    res.render('review_page.html')
-})
-
-app.get('/product_page', function(req, res) {
     res.render('product_page.html')
 })
 
-app.listen(3000, function(){
-    console.log('server started on port 3000...')
+app.get('/login', function(req, res) {
+    res.render('login.html')
+})
+app.get('/register', function(req, res) {
+    res.render('register.html')
+})
+app.get('/product_review', function(req, res) {
+    res.render('review_page.html')
+})
+
+
+
+app.listen(80, function(){
+    console.log('server started on port 80...')
 })
