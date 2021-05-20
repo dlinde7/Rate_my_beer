3180c07f9bb90cb2535a20855081aa88ecb26d11
mmqadi <mmqadi@users.noreply.github.com>
2021-05-20 23:18:46 +0200

    .

--- public/js/review_page.js
+++ public/js/review_page.js
@@ -1,20 +1,20 @@
 function loadReviews() {
   const params = new URLSearchParams(window.location.search);
-  console.log(params.get("id"));
-  var fetchString = "/getproduct?id=" + params.get("id");
+  console.log(params.get('id'));
+  var fetchString = '/getproduct?id=' + params.get('id');
 
   fetch(fetchString)
     .then((response) => response.json())
     .then((data) => {
       let beer = Object();
       beer = data;
-      document.getElementById("name").innerText = beer.Name;
-      document.getElementById("rating").innerText =
-        "Average Rating: " + beer.Rating;
-      document.getElementById("description").innerText = beer.Decription;
+      document.getElementById('name').innerText = beer.Name;
+      document.getElementById('rating').innerText =
+        'Average Rating: ' + beer.Rating;
+      document.getElementById('description').innerText = beer.Decription;
     });
 
-  fetchString = "/get_product_reviews?productId=" + params.get("id");
+  fetchString = '/get_product_reviews?productId=' + params.get('id');
   fetch(fetchString)
     .then((response) => response.json())
     .then((data) => {
@@ -28,7 +28,7 @@ function onBackClicked() {
 }
 
 function createReviewsWithInnerHTML(reviews) {
-  const reviewPlaceholder = document.getElementById("reviewPlaceHolder");
+  const reviewPlaceholder = document.getElementById('reviewPlaceHolder');
   console.log(reviews);
   const rows = reviews.map((review) => {
     if (review.Username) {
@@ -50,13 +50,13 @@ function createReviewsWithInnerHTML(reviews) {
     }
   });
 
-  const html = `${rows.join(" ")}`;
+  const html = `${rows.join(' ')}`;
 
   reviewPlaceholder.innerHTML = html;
 }
 function onRateClicked() {
   const params = new URLSearchParams(window.location.search);
-  var radios = document.getElementsByName("rate");
+  var radios = document.getElementsByName('rate');
   let radioValue = 0;
   for (var i = 0, length = radios.length; i < length; i++) {
     if (radios[i].checked) {
@@ -64,19 +64,20 @@ function onRateClicked() {
       break;
     }
   }
-  const reviewText = document.getElementById("userReview").value;
+  const reviewText = document.getElementById('userReview').value;
 
   const review = {
     userId: 1,
-    beerId: params.get("id"),
+    beerId: params.get('id'),
     rating: radioValue,
     reviewText: reviewText,
   };
 
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", "/add_review", true);
-  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.open('POST', '/add_review', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(JSON.stringify(review));
+  location.reload();
 }
 
 loadReviews();
