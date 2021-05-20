e7f70137d741ab2792adf6a81070aa939000826f
mmqadi <mmqadi@users.noreply.github.com>
2021-05-20 20:09:42 +0200

    .

--- app.js
+++ app.js
@@ -5,43 +5,54 @@ const http = require('http');
 const con = require('./services/db');
 const DbController = require('./controllers/databaseController');
 
-app.set('views', __dirname + '/views')
+app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 app.engine('html', require('ejs').renderFile);
 
 var dbController = new DbController.dbController(con);
 
 app.get('/', function (req, res) {
-    res.render('product_page.html')
-})
+  res.render('product_page.html');
+});
 
 app.get('/login', function (req, res) {
-    res.render('login.html')
-})
+  res.render('login.html');
+});
 
 app.get('/register', function (req, res) {
-    res.render('register.html')
-})
+  res.render('register.html');
+});
 
 app.get('/product_review', function (req, res) {
-    res.render('review_page.html')
-})
+  res.render('review_page.html');
+});
 
 app.get('/getreviews', function (req, res) {
-    let results = new Object();
-    con.connection.query("SELECT * FROM Beer", function (err, result, fields) {
-        if (err) {
-            throw err;
-        }
-        else {
-            results = result;
-            results = JSON.stringify(results);
-            console.log(results);
-            res.end(results)
-        }
-    });
-})
+  let results = new Object();
+  con.connection.query('SELECT * FROM Beer', function (err, result, fields) {
+    if (err) {
+      throw err;
+    } else {
+      results = result;
+      results = JSON.stringify(results);
+      console.log(results);
+      res.end(results);
+    }
+  });
+});
+
+app.get('/getproduct', function (req, res) {
+  var id = req.query.id;
+  var queryString = 'SELECT * FROM ratemybeer.Beer WHERE idBeer=' + id + ';';
+  con.connection.query(queryString, function (err, result, fields) {
+    if (err) {
+      throw err;
+    } else {
+      console.log(result);
+    }
+  });
+});
 
 app.listen(3000, function () {
-    console.log('server started on port 3000...')
-})
+  console.log('server started on port 3000...');
+});
