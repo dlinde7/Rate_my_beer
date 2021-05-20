f8346f6fc061db42962d8d80699f159de191d703
mmqadi <mmqadi@users.noreply.github.com>
2021-05-20 20:43:17 +0200

    .

--- public/js/product_page.js
+++ public/js/product_page.js
@@ -1,12 +1,12 @@
 function loadBeers() {
-  fetch("/getbeers")
+  fetch('/getbeers')
     .then((response) => response.json())
-    .then((data) => createElementsWithInnerHTML(data, "Featured Beer"));
+    .then((data) => createElementsWithInnerHTML(data, 'Featured Beer'));
 }
 
 function createElementsWithInnerHTML(products, message) {
-  const productPlaceholder = document.getElementById("pageContent");
-  const header = document.getElementById("headerText");
+  const productPlaceholder = document.getElementById('pageContent');
+  const header = document.getElementById('headerText');
   console.log(products);
   const rows = products.map((product) => {
     return `<section data-id="${product.idBeer}" onclick="onCardClicked(this)" class="card slit-in-vertical">
@@ -17,16 +17,16 @@ function createElementsWithInnerHTML(products, message) {
               </section>
             </section>`;
   });
-  const html = `${rows.join(" ")}`;
+  const html = `${rows.join(' ')}`;
   productPlaceholder.innerHTML = html;
   header.innerText = message;
 }
 loadBeers();
 
 function onCardClicked(card) {
-  var id = card.getAttribute("data-id");
+  var id = card.getAttribute('data-id');
   console.log(id);
-  window.location.href = "../product_review";
+  window.location.href = '../product_review?id=' + id;
 }
 //SELECT * FROM ratemybeer.Beer where idCompany = {id.idCompany};
 //SELECT * FROM ratemybeer.Beer where Rating => {value};
